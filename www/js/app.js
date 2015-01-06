911640f9d9ffd94df14481261694515b6ea9f4db
munjala <aarti.munjal@ucdenver.edu>
2015-01-06 00:55:32 -0700

    everything works except angularjs with jqm sucks. There is no binding available.

--- www/js/app.js
+++ www/js/app.js
@@ -1,104 +1,15 @@
 
-'use strict';
-
 /*
 * Main BB3 application module
  */
-var bb3App = angular.module('bb3', [])
-    .constant('BB3Constants',{
-        // debugging flag
-        debug: true
-    })
-    .config(['$routeProvider', function($routeProvider) {
-        /*
-        * Main route provider for the study design tab
-         */
-        $routeProvider
+
+(function () {
+    "use strict";
+    // this function is strict...
+}());
 
 
-            .when('/favorites',
-            {templateUrl: 'pages/favorites.html', controller: 'contactInfoController'}
-        )
-
-            .when('/dashboard',
-            {templateUrl: 'pages/dashboard.html', controller: 'stateController' }
-        )
-            .when('/home',
-            {templateUrl: 'partials/home.html'}
-        )
-            .when('/login',
-            {templateUrl: 'partials/loginView.html', controller: 'participantsController' }
-        )
-            .when('/signUp',
-            {templateUrl: 'partials/signUpView.html', controller: 'participantsController' }
-        )
-            .when('/home',
-            {templateUrl: 'partials/home.html'}
-        )
-            .when('/facebook',
-            {templateUrl: 'partials/facebookView.html', controller: 'solutionTypeController' }
-        )
-            .when('/youtube',
-            {templateUrl: 'partials/youtubeView.html', controller: 'participantsController' }
-        )
-            .when('/text',
-            {templateUrl: 'partials/textView.html', controller: 'contactInfoController' }
-        )
-            .when('/tutorial',
-            {templateUrl: 'partials/tutorialView.html', controller: 'predictorsController' }
-        )
-            .when('/aboutUs',
-            {templateUrl: 'partials/aboutUsView.html', controller: 'covariatesController' }
-        )
-
-            // contact us screens
-            .when('/feedback',
-            {templateUrl: 'partials/feedbackView.html', controller: 'feedbackController' }
-
-        )
-
-            .when('/contactInfo',
-            {templateUrl: 'partials/contactInfoView.html', controller: 'contactInfoController' }
-
-        )
-            .when('/spanish/login',
-            {templateUrl: 'partials/spanish/loginView.html', controller: 'participantsSpanishController' }
-        )
-            .when('/spanish/signUp',
-            {templateUrl: 'partials/spanish/signUpView.html', controller: 'participantsSpanishController' }
-        )
-            .when('/spanish/home',
-            {templateUrl: 'partials/spanish/home.html'}
-        )
-            .when('/spanish/facebook',
-            {templateUrl: 'partials/spanish/facebookView.html', controller: 'solutionTypeController' }
-        )
-            .when('/spanish/youtube',
-            {templateUrl: 'partials/spanish/youtubeView.html', controller: 'nominalPowerController' }
-        )
-            .when('/spanish/text',
-            {templateUrl: 'partials/spanish/textView.html', controller: 'contactInfoSpanishController' }
-        )
-            .when('/spanish/tutorial',
-            {templateUrl: 'partials/spanish/tutorialView.html', controller: 'predictorsController' }
-        )
-            .when('/spanish/aboutUs',
-            {templateUrl: 'partials/spanish/aboutUsView.html', controller: 'covariatesController' }
-        )
-
-            // contact us screens
-            .when('/spanish/feedback',
-            {templateUrl: 'partials/spanish/feedbackView.html', controller: 'feedbackController' }
-
-        )
-
-            .when('/spanish/contactInfo',
-            {templateUrl: 'partials/spanish/contactInfoView.html', controller: 'contactInfoController' }
-
-        )
-
-            .otherwise({ redirectTo: '/' });
-    }]);
+var bb3App = angular.module('bb3', []);
 
 
 
--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -4,7 +4,7 @@
  * Controller which manages the completion state of the navbar
  */
 
-var bb3App = angular.module('bb3App', []);
+//var bb3App = angular.module('bb3', []);
 
 bb3App.controller('stateController',
     function($scope, $window) {
@@ -20,12 +20,50 @@ bb3App.controller('stateController',
 
         $scope.getLoginStatus = function() {
             window.loginStatus = $scope.login;
-            return $scope.login;
+            //return $scope.login;
         };
 
         $scope.changeLoginStatus = function() {
             window.alert('called function');
             $scope.login = true;
-            return;
+
         }
-});
+})
+
+    .controller('dashboardController',
+    function ($scope, $window) {
+
+        init();
+        function init() {
+            $scope.status = false;
+        }
+
+        $scope.getStatus = function() {
+
+            $scope.$on("$includeContentLoaded", function(){
+                $("#dashboard").trigger("create");
+            });
+
+            return $scope.status;
+        };
+    })
+
+    .controller('personalInfoController',
+    function ($scope, $window) {
+        init();
+        function init() {
+            $scope.babyName = "default";
+        }
+
+
+        $scope.getBabyName = function() {
+            $scope.babyName = "notDefault";
+            return $scope.babyName;
+        };
+
+        $scope.submitSignupInfo = function() {
+            window.alert('signup pressed');
+
+        };
+
+    });
